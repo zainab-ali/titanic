508d7a259cac06d5f3ad9a2e89d702b277680731
Pierangelo Cecchetto <pierangeloc@gmail.com>
2017-12-16 10:08:23 +0000

    Add missing Dependencies file in `project` folder

---
+++ project/Dependencies.scala
@@ -0,0 +1,7 @@
+import sbt._
+
+object Dependencies {
+  val purecsv = "com.github.melrief" %% "purecsv" % "0.1.1"
+  val matryoshka = "com.slamdata" %% "matryoshka-core" % "0.18.3"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
+}
